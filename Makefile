0f661c53f03a16cad50fccb1464f09e869e8252f
Ehsan Tofigh <ehsan@ampyxpower.com>
2017-12-14 22:07:41 +0100

    fixup! Add linker file

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ INCLUDES = -I$(wildcard src/drivers)
 OBJECTS  = $(addprefix $(BUILDDIR)/, $(addsuffix .o, $(basename $(SOURCES))))
 
 CC       = arm-none-eabi
-CFLAGS   = -g -std=gnu99 -O2 -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -nostartfiles $(INCLUDES)
+CFLAGS   = -g -O1 -std=gnu99 -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -nostartfiles $(INCLUDES)
 LDFLAGS  = -T src/linker.ld
 
 $(BUILDDIR)/%.img: $(BUILDDIR)/$(NAME).elf
--- src/main.c
+++ src/main.c
@@ -1,21 +1,40 @@
 #include "rpi.h"
 
+#define LED_PIN 	16
+
+#define JTAG_TRST 	22
+#define JTAG_TDI	4
+#define JTAG_TDO	24
+#define JTAG_TMS	27
+#define JTAG_TCK	25
+
+void enable_jtag(void)
+{
+	gpio_set_pin_pull(JTAG_TRST, GPIO_PULL_DISABLED);
+	gpio_set_pin_pull(JTAG_TDI, GPIO_PULL_DISABLED);
+	gpio_set_pin_pull(JTAG_TDO, GPIO_PULL_DISABLED);
+	gpio_set_pin_pull(JTAG_TMS, GPIO_PULL_DISABLED);
+	gpio_set_pin_pull(JTAG_TCK, GPIO_PULL_DISABLED);
+
+	gpio_set_function(JTAG_TRST, GPIO_FSEL_ALT4);
+	gpio_set_function(JTAG_TDI, GPIO_FSEL_ALT5);
+	gpio_set_function(JTAG_TDO, GPIO_FSEL_ALT4);
+	gpio_set_function(JTAG_TMS, GPIO_FSEL_ALT4);
+	gpio_set_function(JTAG_TCK, GPIO_FSEL_ALT4);
+}
+
 int kernel_main(unsigned int r0, unsigned int r1, unsigned int atags)
 {
-	while(1) {
-		gpio_clr_pin(16);
+	enable_jtag();
 
-		for(volatile int i = 0; i < 10000; i++);
-
-		gpio_set_pin(16);
-
-		for(volatile int i = 0; i < 10000; i++);
+	gpio_set_function(LED_PIN, GPIO_FSEL_OUTPUT);
+	
+	while (1) {
+		gpio_clr_pin(LED_PIN);
+		for(volatile int i = 0; i < 1000000; i++);
+		gpio_set_pin(LED_PIN);
+		for(volatile int i = 0; i < 1000000; i++);
 	}
 
 	return 0;
-}
-
-void exit(int code)
-{
-    while(1);
 }
